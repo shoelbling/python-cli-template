415e2039b14a3866e3b455e8dee5bc4f68e5e6b9
Sven Hoelbling <sholbling@gmail.com>
2025-07-23 14:10:34 +1000

    Sync latest changes

--- mycli/__main__.py
+++ mycli/__main__.py
@@ -1,11 +1,9 @@
 import sys
 
-
 def main():
-    print("Hello from Python CLI! Welcome to mycli.")
+    print("Hello from Python CLI!")
     if len(sys.argv) > 1:
         print(f"Args: {sys.argv[1:]}")
-
 
 if __name__ == "__main__":
     main()
